dcc3c15836947ba80d33a71d4c1d26a6f42aa0b9
Hardy Chisom Junior <hardyjunior36@gmail.com>
2022-09-08 16:13:36 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *  *main- Serves as the entry point for the program
  *   *
  *    *Return: By default returns zero if no errors otherwise returns non-zero value.
- *     */
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
